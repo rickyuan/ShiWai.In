a99e687240aebf5dc08b70024f09c98854fa6a5a
rickyuan <rushyzt@gmail.com>
2011-12-14 21:55:48 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShiWaiIn::Application.config.secret_token = '0728a157558c2883793045e2a504ab2225cd2cfa2e83a1598bef97a22af2ac27bd89cc26e32c9174a5b500e90b8e47a50e82f9f96554a6f91ae7aade59a1c3c7'

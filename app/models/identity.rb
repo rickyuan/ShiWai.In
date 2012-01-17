cf14a5a3a9902c3ad512173d9333b2b2c5732b2c
rickyuan <rushyzt@gmail.com>
2012-01-17 23:58:35 +0800

    added sina weibo auth header

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -1,5 +1,5 @@
 class Identity < OmniAuth::Identity::Models::ActiveRecord
-  attr_accessible :name, :email, :password_digest, :password_digest_confirmation
+  #attr_accessible :name, :email, :password_digest, :password_digest_confirmation
   validates_presence_of :name
   validates_uniqueness_of :email
   validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
